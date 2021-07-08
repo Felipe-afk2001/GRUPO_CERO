1ac7426c4ad9b059cadfa87ac3de4f47994da7e9
felipe-afk2001 <fe.medinar@duocuc.cl>
2021-07-08 18:43:05 -0400

    avance felipe, modificacion de nadvar

--- GRUPO_ZERO/GRUPO_ZERO/urls.py
+++ GRUPO_ZERO/GRUPO_ZERO/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core.views import index, obras
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('core.url'))
+    path('',include('core.urls')),
+    path('index/',index),
+    path('obras/',obras)
 ]
 
 
--- GRUPO_ZERO/core/urls.py
+++ GRUPO_ZERO/core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import index
+from .views import obras
 
 urlpatterns = [
     path('',index,name="index"),
+    path('',obras,name="obras"),
 ]
